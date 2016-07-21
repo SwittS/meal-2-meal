ccc78d99a1603799fc0d62338309b0e619d65cb8
SwittS <switt.srikulacheep@gmail.com>
2016-07-21 11:13:23 -0700

    revert back to an older version of jquery because of a console log error

--- models/index.js
+++ models/index.js
@@ -1,2 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/meal-2-meal");
+
+var Meal = require('./meal');
+var Ingredient = require('./ingredient');
+
+module.exports.Meal = Meal;
+module.exports.Ingredient = Ingredient;
--- models/meal.js
+++ models/meal.js
@@ -1,10 +1,11 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+var Ingredient = require('./ingredient');
+
 var MealSchema = new Schema({
   name: String,
   songs: [ Ingredient.schema ]
-
 });
 
 var MealSchema = mongoose.model('Meal', MealSchema);
