edd0b2c6f391608d92a95133a1557784f85cd51d
SwittS <switt.srikulacheep@gmail.com>
2016-07-22 13:56:41 -0700

    ready for heroku deploy

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/meal-2-meal");
 
 var Meal = require('./meal');
 // var Ingredient = require('./ingredient');
 
 module.exports.Meal = Meal;
 // module.exports.Ingredient = Ingredient;
+
+mongoose.connect( process.env.MONGODB_URI || "monggodb://localhost/meal-2-meal" );
