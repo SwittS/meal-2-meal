d37100d2343d1c549a1205609142f5416da01774
SwittS <switt.srikulacheep@gmail.com>
2016-07-21 23:32:54 -0700

    create buttons and dropdowns using materialize

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     console.log('app.js loaded!');
 
-    $(".dropdown-button").dropdown();
+    $('.parallax').parallax();
+
+    // $('select').material_select();
+    // $("select[required]").css({display: "inline-block", height: 0, padding: 0, width: 0});
 
     $.get('/api/meals').success(function(meals) {
         meals.forEach(function(meals) {
