e4e61b80420b5e5e89d0e1186abdf0f4c8d77b9e
SwittS <switt.srikulacheep@gmail.com>
2016-07-22 12:03:55 -0700

    remove navbar

--- seed.js
+++ seed.js
@@ -1,28 +1,27 @@
 var db = require("./models");
 
-var meals_list = [
-              {
-                name: " 8oz Chicken Breast and 200g Rice",
-                ingredients: [ 'chicken breast', 'rice']
-              },
-              {
-                name: " 8oz Ribeye Steak and 200g Yukon Potatoes",
-                ingredients: [ 'ribeye steak', 'yukon potato']
-              }
-];
+var meals_list = [{
+    name: " 8oz Chicken Breast and 200g Rice",
+    ingredients: ['chicken breast', 'rice']
+}, {
+    name: " 8oz Ribeye Steak and 200g Yukon Potatoes",
+    ingredients: ['ribeye steak', 'yukon potato']
+}];
 
 // remove all records that match {} -- which means remove ALL records
-db.Meal.remove({}, function(err, meals){
-  if(err) {
-    console.log('Error occurred in remove', err);
-  } else {
-    console.log('removed all meals');
+db.Meal.remove({}, function(err, meals) {
+    if (err) {
+        console.log('Error occurred in remove', err);
+    } else {
+        console.log('removed all meals');
 
-    // create new records based on the array books_list
-    db.Meal.create(meals_list, function(err, meals){
-      if (err) { return console.log('err', err); }
-      console.log("created", meals.length, "meals");
-      process.exit();
-    });
-  }
+        // create new records based on the array books_list
+        db.Meal.create(meals_list, function(err, meals) {
+            if (err) {
+                return console.log('err', err);
+            }
+            console.log("created", meals.length, "meals");
+            // process.exit();
+        });
+    }
 });
