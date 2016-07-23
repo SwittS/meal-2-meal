172fbea43d9763ec8734d09806b4d036b5d5afbd
SwittS <switt.srikulacheep@gmail.com>
2016-07-22 18:33:09 -0700

    fix seed.js data

--- seed.js
+++ seed.js
@@ -3,38 +3,42 @@ var db = require("./models");
 var mealList = [];
 
 mealList.push({
-              name: '8oz Chicken Breast and 200g Rice',
-            });
+    name: '8oz Chicken Breast and 200g Rice',
+});
 mealList.push({
-              name: '8oz Ribeye Steak and 200g Yukon Potatoes',
-            });
+    name: '8oz Ribeye Steak and 200g Yukon Potatoes',
+});
 
 var sampleIngredients = [];
 
-sampleIngredients.push ({ name: 'chicken breast',
-                          carbohydrates: 0,
-                          fats: 2,
-                          protein: 48
+sampleIngredients.push({
+    name: 'chicken breast',
+    carbohydrates: 0,
+    fats: 2,
+    protein: 48
 });
-sampleIngredients.push ({ name: 'rice',
-                          carbohydrates: 57,
-                          fats: 0,
-                          protein: 5
+sampleIngredients.push({
+    name: 'rice',
+    carbohydrates: 57,
+    fats: 0,
+    protein: 5
 });
-sampleIngredients.push ({ name: 'ribeye steak',
-                          carbohydrates: 0,
-                          fats: 52,
-                          protein: 48
+sampleIngredients.push({
+    name: 'ribeye steak',
+    carbohydrates: 0,
+    fats: 52,
+    protein: 48
 });
-sampleIngredients.push ({ name: 'yukon potato',
-                          carbohydrates: 35,
-                          fats: 0,
-                          protein: 4
+sampleIngredients.push({
+    name: 'yukon potato',
+    carbohydrates: 35,
+    fats: 0,
+    protein: 4
 });
 
 // populate each meals ingredients list
 mealList.forEach(function(meal) {
-  meal.ingredients = sampleIngredients;
+    meal.ingredients = sampleIngredients;
 });
 
 
@@ -46,7 +50,7 @@ db.Meal.remove({}, function(err, meals) {
         console.log('removed all meals');
 
         // create new records based on the array
-        db.Meal.create(meals_list, function(err, meals) {
+        db.Meal.create(mealList, function(err, meals) {
             if (err) {
                 return console.log('err', err);
             }
