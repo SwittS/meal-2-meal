8c66f1c6a968dcda96e6cf070ea9066f79a01600
SwittS <switt.srikulacheep@gmail.com>
2016-07-23 13:04:51 -0700

    continuation of seed.js repair, almost there!

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/meal-2-meal" );
 
 var Meal = require('./meal');
 var Ingredient = require('./ingredient');
 
 module.exports.Meal = Meal;
 module.exports.Ingredient = Ingredient;
-
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/meal-2-meal" );
--- seed.js
+++ seed.js
@@ -1,33 +1,24 @@
 var db = require("./models");
 
-var mealList = [{
-    name: '8oz Chicken Breast and 200g Rice'
-}, {
-    name: '8oz Ribeye Steak and 200g Yukon Potatoes'
-}];
-
 var ingredientList = [{
     ingredientName: 'chicken breast',
     mealName: '8oz Chicken Breast and 200g Rice',
     carbohydrates: 0,
     fats: 2,
     protein: 48
-},
-{
-    ingredientNamename: 'rice',
+}, {
+    ingredientName: 'rice',
     mealName: '8oz Chicken Breast and 200g Rice',
     carbohydrates: 57,
     fats: 0,
     protein: 5
-},
-{
+}, {
     ingredientName: 'ribeye steak',
     mealName: '8oz Ribeye Steak and 200g Yukon Potatoes',
     carbohydrates: 0,
     fats: 52,
     protein: 48
-},
-{
+}, {
     ingredientName: 'yukon potato',
     mealName: '8oz Ribeye Steak and 200g Yukon Potatoes',
     carbohydrates: 35,
@@ -35,8 +26,14 @@ var ingredientList = [{
     protein: 4
 }];
 
+var mealList = [{
+    name: '8oz Chicken Breast and 200g Rice'
+}, {
+    name: '8oz Ribeye Steak and 200g Yukon Potatoes'
+}];
+
 db.Meal.remove({}, function(err, meals) {
-    console.log('removed all meals');
+    console.log('removed all authors');
     db.Meal.create(mealList, function(err, meals) {
         if (err) {
             console.log(err);
@@ -44,42 +41,27 @@ db.Meal.remove({}, function(err, meals) {
         }
         console.log('recreated all meals');
         console.log("created", meals.length, "meals");
-
-
-        db.Ingredient.remove({}, function(err, ingredients) {
-            console.log('removed all ingredients');
-            ingredientList.forEach(function (ingredientData) {
-                var ingredient = new db.Ingredient({
-                    ingredientName: ingredientData.ingredientName,
-                    carbohydrates: ingredientData.carbohydrates,
-                    fats: ingredientData.fats,
-                    protein: ingredientData.protein
-                });
-                db.Meal.findOne({
-                    name: ingredientData.meal
-                }, function(err, foundMeal) {
-                    console.log('found meal ' + foundMeal.name + ' for ingredient ' + ingredient.ingredientName);
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    ingredient.meal = foundMeal;
-                    ingredient.save(function(err, savedIngredient) {
-                        if (err) {
-                            return console.log(err);
-                        }
-                        console.log('saved ' + savedIngredient.ingredientName + ' by ' + foundMeal.name);
-                    });
-                });
-            });
-        });
-
     });
 });
 
-
-
-
+db.Ingredient.remove({}, function(err, ingredients) {
+    console.log('removed all ingredients');
+    ingredientList.forEach(function(ingredientData) {
+        var ingredient = new db.Ingredient({
+            ingredientName: ingredientData.ingredientName,
+            carbohydrates: ingredientData.carbohydrates,
+            fats: ingredientData.fats,
+            protein: ingredientData.protein
+        });
+        db.Meal.findOne({name: ingredientData.mealName}, function (err, foundMeal) {
+         console.log('found meal ' + foundMeal.name + ' for meal ' + ingredient.ingredientName);
+         if (err) {
+           console.log(err);
+           return;
+         }
+       });
+     });
+   });
 
 
 
