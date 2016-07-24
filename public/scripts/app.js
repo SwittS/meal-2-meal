a83b57f0c91f8cb6da852faca73f58ddb090f553
SwittS <switt.srikulacheep@gmail.com>
2016-07-24 16:02:26 -0700

    begin work with handlebars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    console.log('app.js loaded!');
+    console.log('Sanity Check: Let\'s Party!');
 
     $('.parallax').parallax();
-    
+    $('select').material_select();
+
+    // compile handlebars template
+    // var source = $('#mealChoices-template').html();
+    // template = Handlebars.compile(source);
+
     $.get('/api/meals').success(function(meals) {
         meals.forEach(function(meals) {
             // res.json(meals);
         });
     });
-
 
 });
