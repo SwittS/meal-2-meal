5ce53c8fc92429c4afdee31339081ea7179e85c7
SwittS <switt.srikulacheep@gmail.com>
2016-07-24 16:52:03 -0700

    first attempt at handlebars and the dropdown menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,24 @@
+console.log('Sanity Check: Let\'s Party!');
+
 $(document).ready(function() {
-    console.log('Sanity Check: Let\'s Party!');
 
     $('.parallax').parallax();
     $('select').material_select();
 
-    // compile handlebars template
-    // var source = $('#mealChoices-template').html();
-    // template = Handlebars.compile(source);
+    function renderMeals(meals) {
+        var mealHtml = $('#t').html();
+        var mealTemplate = Handlebars.compile(mealHtml);
+        var html = mealTemplate(meal);
+        $('#t').prepend(html);
+    }
 
+    function onSuccess(json) {
+        console.log('FOUND ALL MEALS');
+        json.forEach(function(meals) {
+            renderMeals(meals);
+            console.log("SUCCESS rendered the following meals to the page:" + meals);
+        });
+    }
     $.get('/api/meals').success(function(meals) {
         meals.forEach(function(meals) {
             // res.json(meals);
