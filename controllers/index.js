8833b8657cde0a2a54f741777743deb85b2f0105
SwittS <switt.srikulacheep@gmail.com>
2016-07-25 09:59:31 -0700

    change for heroku

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.api = require('./apiController');
-module.exports.meals = require('./mealsController');
 module.exports.mealsIngredients = require('./mealsIngredientsController');
+
+module.exports.meals = require('./mealsController');
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,11 @@
-console.log('Sanity Check: Let\'s Party!');
-
 $(document).ready(function() {
+    console.log('Sanity Check: Let\'s Party!');
 
     $('.parallax').parallax();
     $('select').material_select();
 
-    function renderMeals(meals) {
-        var mealHtml = $('#t').html();
-        var mealTemplate = Handlebars.compile(mealHtml);
-        var html = mealTemplate(meal);
-        $('#t').prepend(html);
-    }
-
-    function onSuccess(json) {
-        console.log('FOUND ALL MEALS');
-        json.forEach(function(meals) {
-            renderMeals(meals);
-            console.log("SUCCESS rendered the following meals to the page:" + meals);
-        });
-    }
     $.get('/api/meals').success(function(meals) {
         meals.forEach(function(meals) {
-            // res.json(meals);
         });
     });
 
