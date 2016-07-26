1e5cf6934b99fcf068664032ab4403d119c32a69
SwittS <switt.srikulacheep@gmail.com>
2016-07-25 21:52:39 -0700

    add functionality to the delete button

--- public/scripts/appmeals.js
+++ public/scripts/appmeals.js
@@ -7,15 +7,37 @@ $(document).ready(function() {
         meals.forEach(function(meals) {
             renderMeal(meals);
         });
+      });
 
 
+            $('.modal-trigger').leanModal();
+
+
+            $('#mealTarget').on('click', '.deleteMeal', handleDeleteMealClick);
 
     });
-});
+
+
 
 function renderMeal(meals) {
     var mealHtml = $('#meal-template').html();
     var mealTemplate = Handlebars.compile(mealHtml);
     var html = mealTemplate(meals);
     $('#mealTarget').append(html);
+}
+
+function handleDeleteMealClick(e) {
+  var mealId = $(this).parents('.meal').data('meal-id');
+  console.log('someone wants to delete meal id=' + mealId );
+  $.ajax({
+    url: '/api/meals/' + mealId,
+    method: 'DELETE',
+    success: handleDeleteMealSuccess
+  });
+}
+
+function handleDeleteMealSuccess(data) {
+  var deletedMealId = data._id;
+  console.log('removing the following meal from the page:', deletedMealId);
+  $('div[data-meal-id=' + deletedMealId + ']').remove();
 }
