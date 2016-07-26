1317bb2a40339fcec7ed4ff74bfedd5d8b3090d3
SwittS <switt.srikulacheep@gmail.com>
2016-07-26 16:32:51 -0700

    revert back to original file names

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
 module.exports.api = require('./apiController');
-module.exports.mealsIngredients = require('./mealsIngredientsController.js');
-module.exports.meals = require('./mealsController.js');
+module.exports.mealsIngredients = require('./mealsIngredientsController');
+module.exports.meals = require('./mealsController');
